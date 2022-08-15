ca114a58afc43dc3f8c5c07c090d39e0610d15ae
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 13:51:13 -0400

    Build MultiMC Script

---
+++ .github/scripts/build-multimc.sh
@@ -0,0 +1,5 @@
+cp -r pack/config multimc/.minecraft
+cp -r pack/journeymap multimc/.minecraft
+cp -r pack/kubejs multimc/.minecraft
+
+zip -r TheTraxPack-MultiMC-${{ env.GitVersion_SemVer }}.zip multimc/* multimc/.minecraft/*
