a40d4895119c58525c8318d95cad548add10cc04
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 14:19:12 -0400

    Up one folder thanks

--- .github/scripts/build-multimc.sh
+++ .github/scripts/build-multimc.sh
@@ -5,4 +5,4 @@ cp -r pack/journeymap multimc/.minecraft
 cp -r pack/kubejs multimc/.minecraft
 
 cd multimc
-zip -r TheTraxPack-MultiMC-$PACK_VERSION.zip * .minecraft/*
+zip -r ../TheTraxPack-MultiMC-$PACK_VERSION.zip * .minecraft/*
