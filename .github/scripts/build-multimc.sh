782bc0d5abcd9e2b70ea29dd977f721fce248e9e
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 14:03:41 -0400

    Test Error

--- .github/scripts/build-multimc.sh
+++ .github/scripts/build-multimc.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-cp -r pack/config multimc/.minecraft
-cp -r pack/journeymap multimc/.minecraft
-cp -r pack/kubejs multimc/.minecraft
+echo $env
 
-zip -r TheTraxPack-MultiMC-${{ env.GitVersion_SemVer }}.zip multimc/* multimc/.minecraft/*
+#cp -r pack/config multimc/.minecraft
+#cp -r pack/journeymap multimc/.minecraft
+#cp -r pack/kubejs multimc/.minecraft
+
+#zip -r TheTraxPack-MultiMC-$env.GitVersion_SemVer.zip multimc/* multimc/.minecraft/*
+
+exit 1
