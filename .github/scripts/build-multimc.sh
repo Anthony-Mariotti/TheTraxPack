10629f101586c01076eb3983ee7cf3c6cd25a0d2
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 14:13:46 -0400

    I goofed and didn't update the version variable

--- .github/scripts/build-multimc.sh
+++ .github/scripts/build-multimc.sh
@@ -4,4 +4,4 @@ cp -r pack/config multimc/.minecraft
 cp -r pack/journeymap multimc/.minecraft
 cp -r pack/kubejs multimc/.minecraft
 
-zip -r TheTraxPack-MultiMC-$env.GitVersion_SemVer.zip multimc/* multimc/.minecraft/*
+zip -r TheTraxPack-MultiMC-$PACK_VERSION.zip multimc/* multimc/.minecraft/*
