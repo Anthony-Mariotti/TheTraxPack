bcf3d542503a10465ca2401683e36840cb4eb9f6
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 14:08:42 -0400

    Re-Enable MultiMC build script

--- .github/scripts/build-multimc.sh
+++ .github/scripts/build-multimc.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-echo $PACK_VERSION
+cp -r pack/config multimc/.minecraft
+cp -r pack/journeymap multimc/.minecraft
+cp -r pack/kubejs multimc/.minecraft
 
-#cp -r pack/config multimc/.minecraft
-#cp -r pack/journeymap multimc/.minecraft
-#cp -r pack/kubejs multimc/.minecraft
-
-#zip -r TheTraxPack-MultiMC-$env.GitVersion_SemVer.zip multimc/* multimc/.minecraft/*
-
-exit 1
+zip -r TheTraxPack-MultiMC-$env.GitVersion_SemVer.zip multimc/* multimc/.minecraft/*
