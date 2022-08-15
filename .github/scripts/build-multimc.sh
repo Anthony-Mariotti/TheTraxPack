33214ce7d0224851c3132e08e1cfa951f207ab9f
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-08-15 14:17:18 -0400

    Attempt to remove multimc subdirectory

--- .github/scripts/build-multimc.sh
+++ .github/scripts/build-multimc.sh
@@ -4,4 +4,5 @@ cp -r pack/config multimc/.minecraft
 cp -r pack/journeymap multimc/.minecraft
 cp -r pack/kubejs multimc/.minecraft
 
-zip -r TheTraxPack-MultiMC-$PACK_VERSION.zip multimc/* multimc/.minecraft/*
+cd multimc
+zip -r TheTraxPack-MultiMC-$PACK_VERSION.zip * .minecraft/*
